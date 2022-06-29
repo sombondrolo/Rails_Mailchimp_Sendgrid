0baa7a491c700418ea3e07f5ee4527d8908bd851
Fer <sombondrolo@gmail.com>
2022-06-29 15:51:42 -0300

    Generated layout:install simple

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -4,5 +4,7 @@ class VisitorsController < ApplicationController
     @owner = Owner.new
     Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
     Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + 'will appear in bold blue'
+    flash[:notice] = 'Welcome!'
+    flash[:alert] = 'My birthday is soon.'
   end
 end
