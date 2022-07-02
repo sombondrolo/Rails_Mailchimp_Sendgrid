cf2447aab07e33ca4eb9334211e67d20028b1372
Fer <sombondrolo@gmail.com>
2022-07-02 14:23:32 -0300

    action_mailer setted in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,24 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "learn-rails_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Added from PDF
+  # email enabled in production
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: Rails.application.secrets.domain_name,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.email_provider_username,
+    password: Rails.application.secrets.email_provider_password
+  }
+  # ActionMailer Config
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  # Added from PDF
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
